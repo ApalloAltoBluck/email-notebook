7a5ebd8d62e256cb1b917b5dc3eca289eab19acc
Oliver Buckley <30906123+ApalloAltoBluck@users.noreply.github.com>
2019-11-21 18:24:41 -0500

    improve single binarize

--- index.py
+++ index.py
@@ -1,14 +1,11 @@
 import numpy as np
 from PIL import Image
 
-im = np.array(Image.open('temp/samplenotes.jpg').convert('L').resize((1240, 1754)))
+im = np.array(Image.open('temp/samplenotes.jpg').convert('L').resize((2480 , 3508)))
 
-th = 150
+th = 115
 im_bool = im > th
 
 threshold = (im > th) * 255
-im_bin_64 = (im > 64) * 255
-im_bin_192 = (im > 192) * 255
 
-im_bin = np.concatenate((im_bin_64, threshold, im_bin_192), axis=1)
-Image.fromarray(np.uint8(im_bin)).save('temp/newimage.png')
+Image.fromarray(np.uint8(threshold)).save('temp/newimage.png')
